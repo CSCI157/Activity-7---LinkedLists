0d287567c75ad2ab201988336083187a9be0dcd3
darwyncook <cook@alfred.edu>
2017-05-08 14:35:22 -0400

    Update social.py

--- social.py
+++ social.py
@@ -1,3 +1,9 @@
+import re 
+
+
+class InvalidSocial(Exception):
+    pass
+
 class SS:
     def __init__(self, social):
         search_obj = re.search(r'^(?!219-09-9999|078-05-1120)(?!666|000|9\d{2})\d{3}-(?!00)\d{2}-(?!0{4})\d{4}$',
